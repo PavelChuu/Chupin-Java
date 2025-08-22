f30d9fb5bce21892f01544f23f6519636b9eecfd
PavelChuu <pavellchu@gmail.com>
2025-08-22 22:54:36 +0300

    TwoNumber.java add check division of a number by 0 ArrayNum.java +forEach

--- src/main/java/com/PavelChuu/ArrayNum.java
+++ src/main/java/com/PavelChuu/ArrayNum.java
@@ -3,9 +3,9 @@ public class ArrayNum {
     public static void main(String[] args){
         int[] nums = new int[] {1,2,3,4,5,6,7,8,9,10};
 
-        for (int i=0; i<10; i++){
-            if (nums[i] % 2 == 0)
-                System.out.println(nums[i]);
+        for (int num:nums){
+            if (num % 2 == 0)
+                System.out.println(num);
         }
     }
 }
--- src/main/java/com/PavelChuu/TwoNumber.java
+++ src/main/java/com/PavelChuu/TwoNumber.java
@@ -6,7 +6,6 @@ public class TwoNumber {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int a,b;
-        float c;
 
         System.out.print("Введите целое число для переменной a: ");
         a = scanner.nextInt();
@@ -17,27 +16,15 @@ public class TwoNumber {
             System.out.println("a = b");
         else if (a > b)
             System.out.println("a > b");
-        else if (a < b )
+        else
             System.out.println("a < b");
 
-        for (int i = 0; i < 4; i++){
-            switch (i) {
-                case 0:
-                    System.out.println(a + "+" + b + "=" + (a + b));
-                    break;
-                case 1:
-                    System.out.println(a + "-" + b + "=" + (a - b));
-                    break;
+        System.out.println(a + "+" + b + "=" + (a + b));
+        System.out.println(a + "-" + b + "=" + (a - b));
+        System.out.println(a + "*" + b + "=" + a * b);
 
-                case 2:
-                    System.out.println(a + "*" + b + "=" + a * b);
-                    break;
-
-                case 3:
-                    c = (float) a / b;
-                    System.out.println(a + "/" + b + "=" + c);
-                    break;
-            }
+        if ( b == 0 )
+            System.out.println("Деление на 0 не возможно");
+        else System.out.println(a + "/" + b + "=" + ((float)a / b));
         }
     }
-}
