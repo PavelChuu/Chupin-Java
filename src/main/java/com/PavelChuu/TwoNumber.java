3531a5969efdd51dda169b0ec7636ff512eca949
PavelChuu <pavellchu@gmail.com>
2025-08-24 10:51:30 +0300

    Entering a non-integer number Entering an empty string

--- src/main/java/com/PavelChuu/TwoNumber.java
+++ src/main/java/com/PavelChuu/TwoNumber.java
@@ -6,12 +6,23 @@ public class TwoNumber {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int a,b;
-
         System.out.print("Введите целое число для переменной a: ");
+        //Проверяем ввод числа. Если оно не целое просим повторный ввод
+        while (!scanner.hasNextInt()) {
+            System.out.print("Неверный ввод. Введите целое число для переменной a:");
+            scanner.next();
+        }
         a = scanner.nextInt();
+
         System.out.print("Введите целое число для переменной b: ");
+        //Проверяем ввод числа. Если оно не целое просим повторный ввод
+        while (!scanner.hasNextInt()) {
+            System.out.print("Неверный ввод. Введите целое число для переменной b:");
+            scanner.next();
+        }
         b = scanner.nextInt();
 
+        //Проводим операции над числами
         if (a == b)
             System.out.println("a = b");
         else if (a > b)
--- src/main/java/com/PavelChuu/TwoString.java
+++ src/main/java/com/PavelChuu/TwoString.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 
 public class TwoString {
     public static void main(String[] args){
+        String a,b;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите строку для переменной a: ");
-        String a = scanner.nextLine();
-        System.out.print("Введите строку для переменной b: ");
-        String b = scanner.nextLine();
+        a = scanner.nextLine();
+        //Проверяем что строка не пустая. Если она пустая, запрашиваем повторный ввод.
+            while (a.isEmpty()) {
+                System.out.print("Строка пустая. Введите строку для переменной a:");
+                a = scanner.nextLine();
+            }
 
+        System.out.print("Введите строку для переменной b: ");
+        b = scanner.nextLine();
+        //Проверяем что строка не пустая. Если она пустая, запрашиваем повторный ввод.
+            while (b.isEmpty()) {
+                System.out.print("Строка пустая. Введите строку для переменной b:");
+                b = scanner.nextLine();
+            }
+        //Сравниваем строки
         if (a.equals(b))
             System.out.println("Строки идентичны");
         else
